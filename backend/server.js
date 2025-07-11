0e64363d6167a436ac7259d7c58131f3a0ff5288
Prateek Thakur <curioussprateek.com>
2025-07-11 23:16:55 +0530

    adding vercel.json file

--- backend/server.js
+++ backend/server.js
@@ -1,44 +1,48 @@
-require("dotenv").config();
-const express = require('express');
-const cors = require('cors');
-// const path = require('path');
-const connectDB = require('./config/db');
-const authRoutes = require('./routes/authRoutes');
-const expenseRoutes = require('./routes/expenseRoutes');
-const incomeRoutes = require('./routes/incomeRoutes');
-const dashboardRoutes = require('./routes/dashboardRoutes');
+const dotenv = require("dotenv");
+const express = require("express");
+const cors = require("cors");
+// const path = require("path");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const expenseRoutes = require("./routes/expenseRoutes");
+const incomeRoutes = require("./routes/incomeRoutes");
+const dashboardRoutes = require("./routes/dashboardRoutes");
 
-
+dotenv.config();
 
 const app = express();
 
-// middleware to handle CORS
-app.use(cors(
-    {
-        origin: process.env.CLIENT_URL || '*',
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        allowedHeaders: ["Content-Type", 'Authorization'],
-    }
-));
+// Use double underscore here
+// const __dirname = path.resolve();
+
+// CORS middleware
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use(express.json());
-
-// console.log("Connecting to DB");
 
 
 connectDB();
 
+// Backend routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/income", incomeRoutes);
 app.use("/api/v1/expense", expenseRoutes);
 app.use("/api/v1/dashboard", dashboardRoutes);
 
 
-// app.use('/uploads', express.static(path.join(__dirname, 'uploads'))); 
+// app.use(express.static(path.join(__dirname, "/frontend/dist")));
+// app.get("*", (_, res) => {
+//   res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+// });
 
 
-const PORT = process.env.PORT || 5000;  
-
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
